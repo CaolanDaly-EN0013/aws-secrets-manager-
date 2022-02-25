1f13e9a733eb59992a35f17dfacb20c6bdfd2c51
CaolanDaly-EN0013 <cdaly@aflac.com>
2022-02-25 13:33:20 +0000

    Initial commit

---
+++ src/test/java/com/aws/secretsmanager/awssecretsmanager/AwsSecretsManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aws.secretsmanager.awssecretsmanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AwsSecretsManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
